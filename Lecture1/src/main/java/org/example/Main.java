d127b64824b9172b6d04daec569dc000ee4a029f
kamal126 <kamalchandra265@gmail.com>
2026-08-16 12:52:52 +0530

    First WebMVC demo

---
+++ Lecture1/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+//        HelloController controller = new HelloController();
+//        String s = controller.Hello();
+//
+//        System.out.println(s);
+    }
+}
